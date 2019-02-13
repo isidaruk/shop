6fd9fd2486b727df94f58cce206995eca59067f7
Ivan Sidaruk <isidaruk@yahoo.com>
2019-02-13 17:17:42 +0300

    Updated Cart model with add and save method

--- cart/cart.py
+++ cart/cart.py
@@ -12,6 +12,26 @@ class Cart(object):
     self.session = request.session
     cart = self.session.get(settings.CART_SESSION_ID)
     if not cart:
-        # save an empty cart in session
+        # Save an empty cart in session.
         cart = self.session[settings.CART_SESSION_ID] = {}
     self.cart = cart
+
+    def add(self, product, quantity=1, update_quantity=False):
+        """
+        Add a product to the cart or update its quantity.
+        """
+
+        # Convert into a string in order to serialize it.
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0,
+                                     'price': str(product.price)}
+        if update_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        # Mark the session as "modified" to make sure it gets saved.
+        self.session.modiffied = True
