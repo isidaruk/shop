1db0f29890894ecec95099f00e4c77e26e6b4c09
Ivan Sidaruk <isidaruk@yahoo.com>
2019-02-14 15:42:33 +0300

    Allow to update product quantities in the cart

--- cart/views.py
+++ cart/views.py
@@ -27,4 +27,8 @@ def cart_remove_view(request, product_id):
 
 def cart_detail_view(request):
     cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(
+            initial={'quantity': item['quantity'], 'update': True}
+        )
     return render(request, 'cart/detail.html', {'cart': cart})
